e152773704863c6c6c126c70fd7974a606bc215e
Ljuliana00 <julianazhu665@gmail.com>
2024-09-26 02:03:42 -0400

    Simulate the data and download the raw data
    modify the name of author and some other details

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Starter folder
+# Analysis of Homeless Deaths By Cause
 
 ## Overview
 
-This repo provides students with a foundation for their own projects associated with *Telling Stories with Data*. You do not need every aspect for every paper and you should delete aspects that you do not need.
-
+This repo provides information and analysis relating to the data of homeless deaths by cause.
 
 ## File Structure
 
@@ -18,13 +17,4 @@ The repo is structured as:
 
 
 ## Statement on LLM usage
-
-Aspects of the code were written with the help of the auto-complete tool, Codriver. The abstract and introduction were written with the help of ChatHorse and the entire chat history is available in inputs/llms/usage.txt.
-
-## Some checks
-
-- [ ] Change the rproj file name so that it's not starter_folder.Rproj
-- [ ] Change the README title so that it's not Starter folder
-- [ ] Remove files that you're not using
-- [ ] Update comments in R scripts
-- [ ] Remove this checklist
+No use from LLM
--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,38 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Toronto Opendata
+# Author: Juliana Zhu
+# Date: 26th September 2024
+# Contact: juliana.zhu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(12138)
 
+simulation <- tibble(
+  #simulate the year of death
+  Year.of.death = sample(2017:2023, size = 100, replace = TRUE),
+  
+  #simulate the causes of deaths
+  Cause_of_death = sample(c("Accident", "Cancer", "Cardiovascular Disease", 
+                            "COVID-19", "Drug toxicity", "Drug Toxicity", "Homicide", 
+                            "Infection", "Other", "Pneumonia", "Suicide", "Unknown"), 
+                          size = 100, replace = TRUE),
+  
+  #simulate the age groups
+  Age_group = sample(c("20-39", "40-59", "60+", "Unknown"), size = 100, replace = TRUE),
+  
+  #simulate the genders
+  Gender = sample(c("Male", "Female"), size = 100, replace = TRUE),
+  
+#Count represents the number of deaths by cause for the corresponding category, which is a count of occurrences
+#using Poisson distribution to simulate the count of deaths
+  Count = rpois(100, lambda = 2) #Assume 2 deaths per year due to that cause
+)
 
-
+head(simulation)
